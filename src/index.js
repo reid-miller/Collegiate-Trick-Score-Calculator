ee2ed417f238c150055db705247ac89ee2d6c0da
Reid Miller <reid.miller@wisc.edu>
2019-12-03 15:28:14 -0600

    Restarting project
    Redid to much simpler set up, then will slowly push trick data to parent to be used by buttons and text field

--- src/index.js
+++ src/index.js
@@ -4,30 +4,71 @@ import './index.css';
 
 import Trick from './Trick'
 
-function addTricks() {
-  console.log("Hello Reid hey hey hey");
+class TrickButton extends React.Component {
+  render() {
+    return (
+      <button className="trick-button">
+        {this.props.name}
+      </button>
+    );
+  }
 }
 
-class Application extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tricks: Array(200).fill(null),
-            size: 0,
+class TrickList extends React.Component {
+  
+  renderTrickButton(name) {
+    return <TrickButton 
+    name={name}
+    />;
+  }
+
+  render() {
+    var arr=["one", "two", "three", "four"];
+        var elements=[];
+        for(var i=0;i<arr.length;i++){
+             // push the component to elements!
+            elements.push(<TrickButton name={ arr[i] } />);
         }
-    }
-
-    render() {
-      return(
-        <h1 id="title">Trick Score Calculator</h1>
-      );
-    }
+    return (
+      <div>
+        <div className="button-row">
+          {this.renderTrickButton("Front flip")}
+          {this.renderTrickButton(1)}
+          {this.renderTrickButton(2)}
+        </div>
+        <div className="button-row">
+          {this.renderTrickButton(3)}
+          {this.renderTrickButton(4)}
+          {this.renderTrickButton(5)}
+        </div>
+        <div className="button-row">
+          {elements}
+        </div>
+      </div>
+    );
+  }
 }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Application />,
-    document.getElementById('root')
-  );
-  
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+      <h1 id="title">Trick Calculator</h1>
+        <div className="game-board">
+          <TrickList />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
