862d66731e62793367add032278db39242098494
Reid Miller <reid.miller@wisc.edu>
2020-01-16 17:17:26 -0600

    Updates

---
+++ precache-manifest.6706123617e1d373c8fe46f676d28af1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff515094bb4563f8b56048dfbd4ed739",
+    "url": "/Collegiate-Trick-Score-Calculator/index.html"
+  },
+  {
+    "revision": "230eae994bd3972dc38c",
+    "url": "/Collegiate-Trick-Score-Calculator/static/css/2.593285ce.chunk.css"
+  },
+  {
+    "revision": "e937650744ca142951bf",
+    "url": "/Collegiate-Trick-Score-Calculator/static/css/main.969ead9a.chunk.css"
+  },
+  {
+    "revision": "230eae994bd3972dc38c",
+    "url": "/Collegiate-Trick-Score-Calculator/static/js/2.28d2d9bb.chunk.js"
+  },
+  {
+    "revision": "e937650744ca142951bf",
+    "url": "/Collegiate-Trick-Score-Calculator/static/js/main.14302c5b.chunk.js"
+  },
+  {
+    "revision": "480cb10394230a3aad93",
+    "url": "/Collegiate-Trick-Score-Calculator/static/js/runtime-main.e91d390a.js"
+  }
+]);
